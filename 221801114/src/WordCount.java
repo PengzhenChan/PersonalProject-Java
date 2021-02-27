4f099c1cc25538799db12ba8cca0add4a742dace
silicon-bond <1245489969@qq.com>
2021-02-27 21:12:48 +0800

    定义成员变量

--- 221801114/src/WordCount.java
+++ 221801114/src/WordCount.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount{
+    private static BufferedReader bufferedReader = null;
+    private static BufferedWriter bufferedWriter = null;
+    private static Map<String, Integer> hashMap = new HashMap<String, Integer>();
+
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        
     }
 }
