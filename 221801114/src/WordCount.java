62d6bfbb97348261d2a3f1948e8c5c8207f741d6
silicon-bond <1245489969@qq.com>
2021-02-27 22:03:47 +0800

    feat:get characters

--- 221801114/src/WordCount.java
+++ 221801114/src/WordCount.java
@@ -12,6 +12,7 @@ public class WordCount{
             System.out.println("命令行参数错误，需要两个文件名！");
             System.exit(0);
         }
+        System.out.println("characters:"+getCharacters(args[0]));
         System.out.println("lines:"+getLines(args[0]));
     }
 
@@ -35,6 +36,27 @@ public class WordCount{
             closeInputStream();
         }
         return lines;
+    }
+
+    public static int getCharacters(String filePath){
+        int characters = 0;
+        try{
+            bufferedReader = new BufferedReader(new FileReader(filePath));
+            String currentLine = bufferedReader.readLine();
+            while (currentLine != null){
+                characters += currentLine.length() + 2;
+                currentLine = bufferedReader.readLine();
+            }
+            characters -= 2;
+        }catch (FileNotFoundException e){
+            System.out.println("未找到文件：" + filePath);
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            closeInputStream();
+        }
+        return characters;
     }
 
     private static void closeInputStream(){
