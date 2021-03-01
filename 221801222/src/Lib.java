34d072c0dd6d4ddd70be827180714fd1306a16d7
Fzuerzmj <794858610@qq.com>
2021-03-01 21:22:46 +0800

    增加：返回words和频率最高单词
    增加：返回words和频率最高单词

--- 221801222/src/Lib.java
+++ 221801222/src/Lib.java
@@ -1,6 +1,10 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Lib
 {
@@ -8,7 +12,8 @@ public class Lib
     private int chars;
     private int words;
     private int lines;
-    private Map<String,Integer> maxWords;
+    private Map<String,Integer> maxCntWords;
+    boolean isDoHandleWordsFunc = false;   //标识handleWord函数是否被运行过
 
 
     /*
@@ -21,19 +26,84 @@ public class Lib
     }
 
     /*
-    行数
+    获取行数
      */
     int getLines()
     {
-        Pattern linePattern = Pattern.compile("(^|\n)\\s*\\S+");
-        lines = 0;
-        Matcher matcher = linePattern.matcher(text);
+        Pattern linePattern = Pattern.compile("(^|\n)\\s*\\S+");   //行数匹配正则表达式
+        lines = 0;   //行数置类
+        Matcher matcher = linePattern.matcher(text);   //匹配
         while (matcher.find()) {
             lines++;
         }
         return lines;
     }
 
+
+
+    
+    /*
+    获取单词总数
+     */
+    int getWords()
+    {
+        if(!isDoHandleWordsFunc)
+            handleWords();
+        return words;
+    }
+
+    /*
+    获取频率最高的10个词
+    返回 map
+     */
+    Map<String,Integer> getMaxCntWords()
+    {
+        if(!isDoHandleWordsFunc)
+            handleWords();
+        return maxCntWords;
+    }
+
+
+    /*
+    辅助函数，handleWords
+    因为统计单词数目和词频一并进行
+     */
+    void handleWords()
+    {
+        words = 0;
+        //博客中的word要求：至少以4个英文字母开头，跟上字母数字符号
+        Pattern wordPattern = Pattern.compile("(^|[^A-Za-z0-9])([a-zA-Z]{4}[a-zA-Z0-9]*)");
+        //Pattern wordPattern = Pattern.compile("(^|\\p{P}|\\s)([a-zA-Z]{4}[a-zA-Z0-9]*)");
+
+        Matcher matcher = wordPattern.matcher(text);   //匹配
+        maxCntWords = new HashMap<String, Integer>();   //初始化Map
+        while(matcher.find())
+        {
+            words++;   //单词数+1
+
+            String midWord = matcher.group(2).toString().toLowerCase();  //取出匹配的word,并统一转化为小写
+            System.out.println(midWord);
+
+            Integer wordsCnt = maxCntWords.get(midWord);
+            if(wordsCnt == null)   //word在文中第一次出现
+            {
+                maxCntWords.put(midWord, 1);
+            }
+            else  //非第一次出现
+            {
+                maxCntWords.put(midWord, wordsCnt + 1);   //在原本计数上+1
+            }
+        }
+
+        //通过stream实现对map排序并取前10个
+        maxCntWords =
+                maxCntWords
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+    }
 
 
 }
