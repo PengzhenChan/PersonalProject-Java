6561b1bc7ae4f83348f42fb75c073a50d88725c9
dqcx2411 <dqcx2411@outlook.com>
2021-03-01 19:46:27 +0800

    feat:countWords

---
+++ 031801124/src/Lib.java
@@ -0,0 +1,120 @@
+/**
+ * @ClassName Lib
+ * @Description TODO
+ * @Author Charley Chen
+ * @DATE 2021/2/27 16:18
+ * @Version 1.0
+ **/
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lib {
+    public void countChars(String inputfileName,String outputfileName)
+    {
+        int charnumber=0;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputfileName));
+            String strLine = null;
+            int lineCount = 0;
+            while(null != (strLine = bufferedReader.readLine())){
+                charnumber+= strLine.length();
+                lineCount++;
+
+
+            }
+            System.out.println("文件字符数为"+charnumber);
+            System.out.println("文件字符数统计成功！");
+            File outfile =new File(outputfileName);
+
+            //if file doesnt exists, then create it
+            if(!outfile.exists()){
+                outfile.createNewFile();
+            }
+
+            //true = append file
+            FileWriter fileWritter = new FileWriter(outfile,true);
+            fileWritter.write(Integer.toString(charnumber));
+            fileWritter.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+        public void countWords(String inputfileName, String outputfileName) {
+            Map<String,Integer> wordarray=new HashMap<String,Integer>();
+            int wordnumber = 0;
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(inputfileName));
+                String strLine = null;
+                int lineCount = 0;
+
+                while (null != (strLine = bufferedReader.readLine())) {
+                    int strindex = 0;
+                    int strlength = strLine.length();
+                    int wordlength = 0;
+                    while (true) {
+                        if(strindex<strlength&&isPartOfWord(strLine.charAt(strindex)) == true) {
+                            wordlength++;
+                            strindex++;
+                        }
+                        else
+                        {
+                            String tempword=strLine.substring(strindex-wordlength,strindex);
+                            strindex++;
+                            if(wordlength>=4)
+                            {
+                                if(wordarray.get(tempword)==null)
+                                wordarray.put(tempword,1);
+                                else
+                                {
+                                    wordarray.put(tempword,wordarray.get(tempword)+1);
+                                }
+                            }
+                            break;
+                        }
+                    }
+
+                    lineCount++;
+
+
+                }
+                wordnumber=wordarray.size(); //wrong
+
+                System.out.println("文件单词数为" + wordnumber);
+                System.out.println("文件单词数统计成功！");
+                File outfile = new File(outputfileName);
+
+                //if file doesnt exists, then create it
+                if (!outfile.exists()) {
+                    outfile.createNewFile();
+                }
+
+                //true = append file
+                FileWriter fileWritter = new FileWriter(outfile, true);
+                fileWritter.write(Integer.toString(wordnumber));
+                fileWritter.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public boolean isPartOfWord(char cha)
+        {
+            if((cha<='9'&&cha>='0')||(cha<='Z'&&cha>='A')||(cha<='z'&&cha>='a'))
+                return true;
+            else
+                return false;
+        }
+
+    public void countMost()
+    {
+
+    }
+
+
+
+
+}
--- 031801124/src/WordCount.java
+++ 031801124/src/WordCount.java
@@ -1,7 +1,33 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
 public class WordCount {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println(123);
+        Lib lib=new Lib();
+        Scanner scan = new Scanner(System.in);
+        //if (args.length == 2) {
+            //String inputfilename = args[0];
+            //String outputfilename = args[1];
+            System.out.println("测试用，请输入输入文件和输出文件。");
+            String inputfilename = scan.nextLine();
+            String outputfilename = scan.nextLine();
+            try {
+                File inputfile = new File(inputfilename);
+                File outputfile = new File(outputfilename);
+            } catch (Exception e) {
+                System.out.println("文件打开失败");
+                System.exit(0);
+            } finally {
+                //lib.countChars(inputfilename,outputfilename);
+                lib.countWords(inputfilename,outputfilename);
+            }
+
+        //} else {
+            //System.out.println("命令输入错误！");
+            //System.exit(0);
+        }
+
     }
-}
+//}
