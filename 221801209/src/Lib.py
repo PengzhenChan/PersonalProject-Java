0f15a949cc00075c57930e3c18c19d892f78dcaa
ccreater <1126774947@qq.com>
2021-03-03 02:40:43 +0800

    完成topWord

--- 221801209/src/Lib.py
+++ 221801209/src/Lib.py
@@ -8,6 +8,7 @@ class WordCount:
         if not type(s) == str:
             raise TypeError("请传入一个字符串")
         self.str = s
+        self.words = []
 
     def charCount(self):
         """
@@ -23,13 +24,24 @@ class WordCount:
         """
         s = self.str
         s = s.lower()
-        return len(re.findall("([a-z]{4,}[a-z0-9]*)",s))
+        self.words = re.findall("([a-z]{4,}[a-z0-9]*)", s)
+
+        return len(self.words)
 
     def topWord(self):
         """
         Returns:
             [[单词，频率]...]
         """
+        if self.words == []:
+            self.wordCount()
+        count = []
+        keys = list(set(self.words))
+        for key in keys:
+            count.append([key, self.words.count(key)])
+        count.sort(key=(lambda e: e[1]))
+        return count[:10]
+
 
     def lineCount(self):
         """
@@ -38,13 +50,17 @@ class WordCount:
         """
         s = self.str
         # 去除空白字符\f\n\r\t\v\b
-        spaces = "\f\n\r\t\v\b"
+        spaces = "\f\t\v\b"
         for space in spaces:
             s = s.replace(space, "")
 
         # 将\r\n和\r式的换行转化为\n式
         s = s.replace("\r", "\n")
-        s = s.replace("\n\n", "\n")
+        while True:
+            tmp = s.replace("\n\n", "\n")
+            if tmp == s:
+                break
+            s = tmp
 
         # 标准化完成,行数计算
-        return s.count("\n")
+        return s.count("\n") + 1
