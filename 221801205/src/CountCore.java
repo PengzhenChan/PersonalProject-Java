788c48b65ed02d0842f02256df694a192d9c377d
gz321 <2582086674@qq.com>
2021-03-02 17:15:12 +0800

    add func isWord

--- 221801205/src/CountCore.java
+++ 221801205/src/CountCore.java
@@ -8,4 +8,19 @@ public class CountCore {
     public int getCharCount(String words) {
     	return words.length();
     }
+    
+    /*
+     * 功能：判断只包含数字和字母的字符串是否为单词
+     * 参数：字符串
+     * 返回值：单词数
+     */
+    public boolean isWord(String s) {
+    	char[] c = s.toCharArray();
+    	for (int i = 0 ; i < 4 ; i ++) {
+    		if (!(Character.isUpperCase(c[i]) || Character.isLowerCase(c[i]))) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
 }
