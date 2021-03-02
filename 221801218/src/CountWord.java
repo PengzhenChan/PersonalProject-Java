d25375e40e9e2144a3b2c1efebcd94f34390e09d
Zheng Linxuan <zhenglinxuan@hotmail.com>
2021-03-02 22:46:56 +0800

    - 增加获取词频Top 10功能

--- 221801218/src/CountWord.java
+++ 221801218/src/CountWord.java
@@ -1,8 +1,11 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.*;
 
 public class CountWord {
     FileInputStream inputStream;
+    Map<String, Integer> words = new HashMap<>();
+
     int count = 0;
 
     public CountWord(FileInputStream in) {
@@ -13,17 +16,49 @@ public class CountWord {
         String str = "";
         int i;
         while ((i = inputStream.read()) != -1) {
-            if (i == ' ' || !Character.isLetterOrDigit(i)) {
-                str = "";
-            } else {
+            if (Character.isLetterOrDigit(i)) {
                 str = str + (char)i;
-            }
-
-            if (!str.isEmpty() && isWord(str)) {
-                count++;
+            } else {
+                if (isWord(str)) {
+                    count++;
+                    if (!words.containsKey(str)) {
+                        words.put(str.toLowerCase(), 1);
+                    } else {
+                        int value = words.get(str.toLowerCase());
+                        words.put(str, value + 1);
+                    }
+                    str = "";
+                }
             }
         }
         return count;
+    }
+
+    public LinkedHashMap<String, Integer> MostFreqWord() {
+        if (words.isEmpty()) {
+            return null;
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(words.entrySet());
+
+        list.sort(new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                int re = e2.getValue().compareTo(e1.getValue());
+                if (re != 0) {
+                    return re;
+                } else {
+                    return e1.getKey().compareTo(e2.getKey());
+                }
+            }
+        });
+
+        LinkedHashMap<String, Integer> topWords = new LinkedHashMap<>();
+
+        for (int i = 0; i < 10; i++){
+            topWords.put(list.get(i).getKey(), list.get(i).getValue());
+        }
+
+        return topWords;
     }
 
     private boolean isWord(String str) {
