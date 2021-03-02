97035057e20939fd930902a677020c30eb8811fe
ccreater <1126774947@qq.com>
2021-03-03 00:50:07 +0800

    完成API设计

---
+++ 221801209/src/Lib.py
@@ -0,0 +1,23 @@
+class WordCount:
+    def __init__(self, s):
+        """
+        Args:
+            s: 待处理字符串
+        """
+        self.str = s
+
+    def charCount(self):
+        """
+        Returns:
+            字符数量
+        """
+    def wordCount(self):
+        """
+        Returns:
+            单词数量
+        """
+    def topWord(self):
+        """
+        Returns:
+            {单词:频率...}
+        """
