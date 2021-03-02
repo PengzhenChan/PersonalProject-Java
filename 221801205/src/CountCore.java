8faca8a7ed13d0613397149bfbc91936dd531797
gz321 <2582086674@qq.com>
2021-03-02 20:18:30 +0800

    add function getWordCount

--- 221801205/src/CountCore.java
+++ 221801205/src/CountCore.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class CountCore {
 	/*
@@ -14,13 +15,66 @@ public class CountCore {
      * 参数：字符串
      * 返回值：单词数
      */
-    public boolean isWord(String s) {
+    private boolean isWord(String s) {
+    	if (s.length() < 4)
+    		return false;
     	char[] c = s.toCharArray();
-    	for (int i = 0 ; i < 4 ; i ++) {
+    	for (int i = 0; i < 4; i ++) {
     		if (!(Character.isUpperCase(c[i]) || Character.isLowerCase(c[i]))) {
     			return false;
     		}
     	}
     	return true;
+    }
+    
+    /*
+     * 功能：判断x是否为分隔符
+     */
+    private boolean isDivision(char x) {
+    	return !(Character.isUpperCase(x) || Character.isLowerCase(x) || Character.isDigit(x));
+    }
+    
+    /*
+     * 功能：将字符串转换为只含数字和字母字符串的数组
+     * 参数：字符串
+     * 返回值：ArrayList<String>
+     */
+    private ArrayList<String> getWordList(String s){
+    	ArrayList<String> wordList = new ArrayList<String>();
+    	char[] c = s.toCharArray();
+    	String word = "";
+    	boolean canAdd = false;
+    	for (char i : c) {
+    		if (!isDivision(i)) {
+    			word += i;
+    			canAdd = true;
+    		}
+    		else {
+    			if (canAdd == true) {
+    				wordList.add(word);
+    				word = "";
+    				canAdd = false;
+    			}		
+    		}
+    	}
+    	if (canAdd == true) 
+    		wordList.add(word);
+    	return wordList;
+    }
+    
+    /*
+     * 功能：统计字符数组的字符串的单词总数
+     * 参数：字符串
+     * 返回值：单词总数
+     */
+    public int getWordCount(String s) {
+    	int cnt = 0;
+    	ArrayList<String> wordList = getWordList(s);
+    	for (String word : wordList) {
+    		if (isWord(word)) {
+    			cnt ++;
+    		}
+    	}
+    	return cnt;
     }
 }
