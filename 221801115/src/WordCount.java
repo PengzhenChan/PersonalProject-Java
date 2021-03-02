ee33365d596c6366173e6fe098b792e8579c01c9
lmh <921509141@qq.com>
2021-03-02 19:54:26 +0800

    添加统计有效行数功能

--- 221801115/src/WordCount.java
+++ 221801115/src/WordCount.java
@@ -3,36 +3,76 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class WordCount {
+public class WordCount 
+{
     private static String inputPath;/*输入路径*/
     private static String outputPath;/*输出路径*/
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+    {
         inputPath = "C:\\Users\\Lin Minghao\\Desktop\\" + args[0];
         outputPath = "C:\\Users\\Lin Minghao\\Desktop\\" + args[1];
-        countCharacters(inputPath);
+        //countCharacters(inputPath);
+        //countLines(inputPath);
         writeFile(outputPath);
 	}
 
-    public static int countCharacters(String inputPath) {
+    public static int countCharacters(String inputPath) 
+    {
         int characters = 0;
-        try {
+        try 
+        {
             BufferedReader br = new BufferedReader(new FileReader(inputPath));
-            while ((br.read()) != -1) {
+
+            while ((br.read()) != -1) 
+            {
                 characters++;
             }
+
             br.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e) 
+        {
             e.printStackTrace();
         }
         return characters;
     }
 
-    public static void writeFile(String outputPath) {
-        try {
+    public static int countLines(String inputPath) 
+    {
+        int lines = 0;
+        try 
+        {
+            BufferedReader br = new BufferedReader(new FileReader(inputPath));
+            String tmp;
+            
+            while ((tmp = br.readLine()) != null) 
+            {
+                if (!tmp.equals("")) 
+                {
+                    lines++;
+                }
+            } 
+
+            br.close();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void writeFile(String outputPath) 
+    {
+        try 
+        {
             BufferedWriter bw = new BufferedWriter(new FileWriter(outputPath));
             bw.write("characters: " + countCharacters(inputPath));
+            bw.write("\nlines: " + countLines(inputPath));
             bw.close();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             e.printStackTrace();
         }
     }
