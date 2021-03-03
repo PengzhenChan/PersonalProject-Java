dcb21756741f16c1ae417966ad87fb3bf7b10a64
sanluo <1296875910@qq.com>
2021-03-03 15:48:46 +0800

    第十次commit

--- 221801110/src/Frequency.java
+++ 221801110/src/Frequency.java
@@ -4,15 +4,24 @@ import java.util.*;
 
 public class Frequency {
 
+    //指定文件路径和文件名
+    private static String path;
+
     //定义一个map集合保存单词和单词出现的个数
     private TreeMap<String,Integer> tm;
 
+
     public Frequency(String path) {
+        this.path = path;
+
         File file=new File(path);
+        Reader readFile = null;
         BufferedReader bufferedReadFile = null;
         String tempString = null;
         try {
-            bufferedReadFile = new BufferedReader(new FileReader(file));
+            readFile = new InputStreamReader(new FileInputStream(file),"UTF-8");
+            bufferedReadFile = new BufferedReader(readFile);
+            
             tm = new TreeMap<String,Integer>();
             //读取文件
             while ((tempString=bufferedReadFile.readLine())!=null){
@@ -20,7 +29,7 @@ public class Frequency {
                 String reg1 = "[^a-zA-Z0-9]";
                 String reg2 ="[a-z]{4}[a-z0-9]*";
                 //将读取的文本进行分割
-                String str[] = tempString.split(reg1);
+                String[] str = tempString.split(reg1);
                 for (String s: str){
                     if (s.matches(reg2)){
                         //判断集合中是否已经存在该单词，如果存在则个数加1，否则将单词添加到集合中，且个数置为1
@@ -39,12 +48,27 @@ public class Frequency {
             System.out.println("文件不存在");
         }
         finally {
-            //printResult(tm);
+            printResult(tm);
         }
     }
 
     public static void printResult(Map<String,Integer> map) {
-        //
+        List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return (o2.getValue().compareTo(o1.getValue()) ); //降序排序,当o2小于、等于、大于o1时，返回-1,0,1
+            }
+        });
+
+        int num = list.size();
+        //System.out.println(num);
+        for (int i = 0; i < num && i < 10; i++) {
+            Map.Entry<String,Integer> entry = list.get(i);
+            if (entry == null) {
+                return;
+            }
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
