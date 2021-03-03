4d5f1677ff748a1b58bc5707419bc9f097005f75
swwsiwwe <215351620@qq.com>
2021-03-04 00:00:13 +0800

    Update CountCore.java

--- 221801234/src/CountCore.java
+++ 221801234/src/CountCore.java
@@ -78,41 +78,74 @@ public class CountCore {
         return 0;
     }
 
-    void quick_sort(String[] list, int l, int r) {
+    void quickSort(String[] list, int l, int r) {
         if(l>=r) return;
         int i = l, j = r;
         int k = i+1;
         int x=map.get(list[l]);
-        if(k<j){
-            while (k<=j) {
-                if (map.get(list[k]) < x){
-                    String tp = list[j];
-                    list[j] = list[k];
-                    list[k] = tp;
-                    j--;
-                }
-                else if (map.get(list[k]) > x){
-                    String tp = list[i];
-                    list[i] = list[k];
-                    list[k] = tp;
-                    k++;
-                    i++;
-                }else{
-                    k++;
-                }
-            }
-            if(r-l>20){
-                quick_sort(list, l, i - 1);
-            }else{
-                quick_sort(list, l, i - 1);
-                quick_sort(list, j + 1, r);
+        while (k <= j) {
+            if (map.get(list[k]) < x) {
+                String tp = list[j];
+                list[j] = list[k];
+                list[k] = tp;
+                j--;
+            } else if (map.get(list[k]) > x) {
+                String tp = list[i];
+                list[i] = list[k];
+                list[k] = tp;
+                k++;
+                i++;
+            } else {
+                k++;
             }
         }
+        quickSort(list, l, i - 1);
+        quickSort(list, j + 1, r);
+    }
+
+    void quickSort_1(String[] list, int l, int r) {
+        if(l>=r) return;
+        int i = l, j = r;
+        int k = i+1;
+        String x=list[l];
+        while (k <= j) {
+            if (list[k].compareTo(x)>0) {
+                String tp = list[j];
+                list[j] = list[k];
+                list[k] = tp;
+                j--;
+            } else if (list[k].compareTo(x)<0) {
+                String tp = list[i];
+                list[i] = list[k];
+                list[k] = tp;
+                k++;
+                i++;
+            } else {
+                k++;
+            }
+        }
+        quickSort_1(list, l, i - 1);
+        quickSort_1(list, j + 1, r);
+
     }
 
     public String[] getPopularWord() {
         String[] list = map.keySet().toArray(new String[0]);
-        quick_sort(list, 0, list.length - 1);
+        quickSort(list, 0, list.length - 1);
+        int cnt=0;
+        int x=0;
+        int integer = map.get(list[0]);
+        for(int i=0;i<list.length;i++){
+            if(map.get(list[i])!=integer) {
+                quickSort_1(list, x, i - 1);
+                cnt += i-x;
+                x=i;
+                integer = map.get(list[i]);
+            }
+            if(cnt>10)
+                break;
+        }
+        quickSort_1(list,x,list.length-1);
         return list;
     }
 }
