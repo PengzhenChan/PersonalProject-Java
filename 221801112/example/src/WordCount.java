7d3043876c8eeff076076e26e7e0bff6ada844c6
Fzucaoxin <954876644@qq.com>
2021-03-03 08:31:59 +0800

    完成统计文件的字符数
    只需要统计Ascii码，汉字不需考虑
    空格，水平制表符，换行符，均算字符

---
+++ 221801112/example/src/WordCount.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @Description: 对输入文件进行字符数，单词数，行数等进行统计
+ * @Author: 曹鑫
+ * @Date: 2021/2/28
+ */
+class WordCount
+{
+    public static void main(String[] args)
+    {
+        WordCount wc = new WordCount();
+        //            wc.outPut(wc.count(args[0]),args[1]);
+        wc.count(args[0]);
+    }
+
+    /**
+     * @Description:  进行字符数，单词数，行数等进行统计
+     * @Param: [input]
+     * @return: java.lang.String
+     * @Date: 2021/2/28
+     */
+    public String count(String input)
+    {
+        StringBuilder result = new StringBuilder(10);
+        //打开输入文件
+        File file = new File(input);
+        try
+        {
+            FileInputStream inputStream=new FileInputStream(file);
+            result.append(getCharNum(file,inputStream));
+            System.out.println("程序计算的答案" + result);
+            inputStream.close();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    /**
+     * @Description:  统计文件的字符数
+     * @Param: [file, inputStream]
+     * @return: java.lang.String
+     * @Date: 2021/2/28
+     */
+    public String getCharNum(File file,FileInputStream inputStream)
+    {
+        return "characters: " + String.valueOf(file.length()) + "\n";
+    }
+
+}
