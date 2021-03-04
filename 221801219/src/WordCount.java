17f93f486715947af02aa94649572d211cc63648
221801219 <18705919793@163.com>
2021-03-04 21:12:05 +0800

    output file

--- 221801219/src/WordCount.java
+++ 221801219/src/WordCount.java
@@ -1,9 +1,15 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class WordCount {
     public static void main(String[] args) {
-        String content = WordCountUtil.readFileByChars("C:\\Users\\Admin\\Desktop\\3.txt");
-        HashMap<String,Integer> unSort = WordCountUtil.countWords(content);
-        WordCountUtil.getTop10Words(unSort);
+        WordCountUtil wcu=new WordCountUtil();
+        Map<String,Integer> hadSorted;
+        String content = wcu.readFileByChars("C:\\Users\\Admin\\Desktop\\3.txt");
+        HashMap<String,Integer> unSort = wcu.countWords(content);
+        hadSorted = wcu.getTop10Words(unSort);
+        wcu.getValueLines(content);
+        wcu.writeFile("C:\\Users\\Admin\\Desktop\\4.txt", hadSorted);
+
     }
 }
--- 221801219/src/WordCountUtil.java
+++ 221801219/src/WordCountUtil.java
@@ -2,21 +2,23 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class WordCountUtil {
+    int charsNumber = 0;
+    int wordNumber = 0;
+    int lineNumber = 0;
     /**
      * 读取文件并统计字符数
      * @param filePath 文件路径
      * @return 文件内容
      */
-    public static String readFileByChars(String filePath) {
+    public String readFileByChars(String filePath) {
         File file = new File(filePath);
         Reader reader = null;
-        //统计字符数
-        int cnt = 0;
         //存储文件内容
         StringBuilder fileContent = new StringBuilder();
         try {
@@ -27,13 +29,12 @@ public class WordCountUtil {
                     fileContent.append((char) temp);
                     //不考虑汉字
                     if(temp < 128) {
-                        cnt++;
+                        charsNumber++;
                     }
                 }
             }
             reader.close();
             System.out.println(fileContent.toString());
-            System.out.println("共有字符数:" + cnt);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,7 +46,7 @@ public class WordCountUtil {
      * @param str 待处理文本
      * @return 单词的哈希表
      */
-    public static HashMap<String, Integer> countWords(String str)
+    public HashMap<String, Integer> countWords(String str)
     {
         String pattern = "(^|[^A-Za-z0-9])([a-zA-Z]{4}[a-zA-Z0-9]*)";
 
@@ -54,11 +55,10 @@ public class WordCountUtil {
 
         // 现在创建 matcher 对象
         Matcher m = r.matcher(str);
-        int cnt = 0;
         HashMap<String, Integer> wordsCount = new HashMap<String, Integer>();
         String temp;
         while (m.find( )) {
-            cnt++;
+            wordNumber++;
             temp = m.group(2).toString().toLowerCase();
             wordsCount.merge(temp, 1, Integer::sum);
         }
@@ -66,7 +66,7 @@ public class WordCountUtil {
         return wordsCount;
     }
 
-    public static Map<String, Integer> getTop10Words(HashMap<String, Integer> m)
+    public Map<String, Integer> getTop10Words(HashMap<String, Integer> m)
     {
         final Map<String, Integer> result = m
                     .entrySet()
@@ -82,7 +82,41 @@ public class WordCountUtil {
         return result;
     }
 
+    public void getValueLines(String str)
+    {
+        Pattern linePattern = Pattern.compile("\\S+");
+        Matcher matcher = linePattern.matcher(str);
+        while (matcher.find()) {
+            lineNumber++;
+            System.out.println(matcher.toString());
+        }
+    }
 
+    public void writeFile(String filePath, Map<String, Integer> hadSorted)
+    {
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                System.out.println("Error!");
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("characters: " + charsNumber + '\n');
+            bw.write("words: " + wordNumber + '\n');
+            bw.write("lines: " + lineNumber + '\n');
+            for (Map.Entry<String, Integer> entry : hadSorted.entrySet()){
+
+                bw.write(entry.getKey()+": "+entry.getValue()+'\n');
+            }
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
