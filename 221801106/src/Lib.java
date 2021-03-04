16fa8302a52222b08c7b7c6703dad55de30e2f6a
郑龙海 <79756877+Hector2333@users.noreply.github.com>
2021-03-04 23:35:47 +0800

    开始编写Lib.java工具类，写了一些变量

--- 221801106/src/Lib.java
+++ 221801106/src/Lib.java
@@ -1,15 +1,19 @@
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
 import java.io.IOException;
 import java.io.PrintWriter;
  
 public class Lib
 {
-	
+	public int wordlines;   //用来记录行数
+	public int characters;    //用来记录字符数
+	public int wordnumbers;    //用来记录单词数
+	public String anticle;    //用来保存从文件读入的全部内容
+	public String inputTxt;    //写入文章的地址
+	public String outputTxt;    //写出文章统计的地址
+    public List<Map.Entry<String, Integer>> list;    //用来记录统计出来的频率排行
+    public HashMap<String,Integer> hashMap;    //用来记录所有单词及在文章中出现的频率
+    public String result;    //用来记录要输出的结果
 }
 
