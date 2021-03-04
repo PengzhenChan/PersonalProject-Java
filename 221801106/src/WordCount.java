32f47130dfe1745dcf17724362931ebe5042a5b5
郑龙海 <79756877+Hector2333@users.noreply.github.com>
2021-03-04 23:08:59 +0800

    删除了所有相关代码，重新开始提交

--- 221801106/src/WordCount.java
+++ 221801106/src/WordCount.java
@@ -1,6 +1,5 @@
 public class WordCount {
     public static void main (String[] args) {
-    	Lib a = new Lib("C:\\Users\\29847\\Desktop\\²âÊÔ.txt","C:\\Users\\29847\\Desktop\\½á¹û.txt");
-    	a.work();
+    	
     }
 }
