0d2eeac16cc8daa91037ad78d18fa88d68621772
郑龙海 <79756877+Hector2333@users.noreply.github.com>
2021-03-04 17:08:51 +0800

    一次性把代码提交，后面删掉分次提交

--- 221801106/src/WordCount.java
+++ 221801106/src/WordCount.java
@@ -1,1 +1,6 @@
-
+public class WordCount {
+    public static void main (String[] args) {
+    	Lib a = new Lib("C:\\Users\\29847\\Desktop\\²âÊÔ.txt","C:\\Users\\29847\\Desktop\\½á¹û.txt");
+    	a.work();
+    }
+}
