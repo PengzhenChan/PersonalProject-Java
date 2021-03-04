d69d16bfe0be5063fe38ff08509c525b534524fe
camocd <6410942@qq.com>
2021-03-04 11:58:39 +0800

    从命令行参数获取文件名,创建Lib处理类

---
+++ 221801230/src/Lib.java
@@ -0,0 +1,30 @@
+public class Lib {
+
+
+    private String inputFile;
+    private String outputFile;
+
+    public Lib() {
+    }
+
+    public Lib(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+
+    public String getInputFile() {
+        return inputFile;
+    }
+
+    public void setInputFile(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public String getOutputFile() {
+        return outputFile;
+    }
+
+    public void setOutputFile(String outputFile) {
+        this.outputFile = outputFile;
+    }
+}
