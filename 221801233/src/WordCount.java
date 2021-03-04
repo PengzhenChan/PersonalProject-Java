70aa3e439c8e70d975a62e136bda95cd366dc921
PengzhenChan <937377257@qq.com>
2021-03-04 12:36:47 +0800

    把输出功能完善了

--- 221801233/src/WordCount.java
+++ 221801233/src/WordCount.java
@@ -1,33 +1,41 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class WordCount {
-    String outfile;
+    String outfiletxt;
+    String infiletxt;
     String str;
-    File file;
+    File infile;
+    File outfile;
     BufferedReader br;
+    BufferedWriter bw;
+    Lib lib;
     public  static void main(String args[])throws Exception{
-        new WordCount(args[0],"a");
+        if(args.length!=2){
+            System.out.println("Required ARGS NUM Error");
+        }
+        else
+        new WordCount(args[0],args[1]);
     }
     public WordCount(String a,String b)throws Exception{
-        outfile=b;
+        outfiletxt=b;
+        infiletxt=a;
         //infile="inputFile.txt";
-        file = new File(a);
+        infile = new File(infiletxt);
+        outfile =new File(outfiletxt);
         in();
-        Lib lib =new Lib(str);
-        System.out.println(str);
+        lib =new Lib(str);
+        //System.out.println(str);
         System.out.println(lib.getLines());
         System.out.println(lib.getChars());
         System.out.println(lib.getWordsNum());
         lib.getTopWords();
+        out();
     }
     public void in() throws IOException {
         try {
             StringBuilder stringBuilder=new StringBuilder();
             String lineword="";
-            br = new BufferedReader(new FileReader(file));
+            br = new BufferedReader(new FileReader(infile));
             while((lineword=br.readLine())!=null){
                 stringBuilder.append(lineword);
                 stringBuilder.append('\n');
@@ -38,6 +46,20 @@ public class WordCount {
         }
         finally{
             br.close();
+        }
+    }
+    public void out() throws IOException {
+        try{
+            bw=new BufferedWriter(new FileWriter(outfile));
+            bw.write("characters:"+lib.getChars()+"\n");
+            bw.write("Words:"+lib.getWordsNum()+"\n");
+            bw.write("lines:"+lib.getLines()+"\n");
+            bw.write(lib.getTopWords());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            bw.close();
         }
     }
 
