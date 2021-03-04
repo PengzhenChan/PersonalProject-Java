a213b394f1392fe6243244c8592606533572bab6
221801219 <18705919793@163.com>
2021-03-04 21:31:32 +0800

    interface

--- 221801219/src/WordCount.java
+++ 221801219/src/WordCount.java
@@ -3,13 +3,12 @@ import java.util.Map;
 
 public class WordCount {
     public static void main(String[] args) {
-        WordCountUtil wcu=new WordCountUtil();
-        Map<String,Integer> hadSorted;
-        String content = wcu.readFileByChars("C:\\Users\\Admin\\Desktop\\3.txt");
-        HashMap<String,Integer> unSort = wcu.countWords(content);
-        hadSorted = wcu.getTop10Words(unSort);
-        wcu.getValueLines(content);
-        wcu.writeFile("C:\\Users\\Admin\\Desktop\\4.txt", hadSorted);
+        WordCountUtil wcu=new WordCountUtil("C:\\Users\\Admin\\Desktop\\3.txt"
+                , "C:\\Users\\Admin\\Desktop\\4.txt");
+        wcu.readFileByChars();
+        wcu.countWords();
+        wcu.getValueLines();
+        wcu.writeFile();
 
     }
 }
--- 221801219/src/WordCountUtil.java
+++ 221801219/src/WordCountUtil.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,18 +10,27 @@ public class WordCountUtil {
     int charsNumber = 0;
     int wordNumber = 0;
     int lineNumber = 0;
+    HashMap<String, Integer> wordsCount;
+    String content="";
+    String inputFilePath="";
+    String outputFilePath="";
+
+    WordCountUtil(String inputPath,String outputPath)
+    {
+        inputFilePath = inputPath;
+        outputFilePath = outputPath;
+    }
     /**
      * 读取文件并统计字符数
      * @param filePath 文件路径
      * @return 文件内容
      */
-    public String readFileByChars(String filePath) {
-        File file = new File(filePath);
-        Reader reader = null;
+    public void readFileByChars() {
+        File file = new File(inputFilePath);
         //存储文件内容
         StringBuilder fileContent = new StringBuilder();
         try {
-            reader = new InputStreamReader(new FileInputStream(file));
+            Reader reader = new InputStreamReader(new FileInputStream(file));
             int temp = -1;
             while ((temp = reader.read()) != -1) {
                 if (((char) temp) != '\r') {
@@ -33,12 +41,11 @@ public class WordCountUtil {
                     }
                 }
             }
+            content = fileContent.toString();
             reader.close();
-            System.out.println(fileContent.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return fileContent.toString();
     }
 
     /**
@@ -46,7 +53,7 @@ public class WordCountUtil {
      * @param str 待处理文本
      * @return 单词的哈希表
      */
-    public HashMap<String, Integer> countWords(String str)
+    public void countWords()
     {
         String pattern = "(^|[^A-Za-z0-9])([a-zA-Z]{4}[a-zA-Z0-9]*)";
 
@@ -54,48 +61,37 @@ public class WordCountUtil {
         Pattern r = Pattern.compile(pattern);
 
         // 现在创建 matcher 对象
-        Matcher m = r.matcher(str);
-        HashMap<String, Integer> wordsCount = new HashMap<String, Integer>();
+        Matcher m = r.matcher(content);
+        wordsCount = new HashMap<String, Integer>();
         String temp;
         while (m.find( )) {
             wordNumber++;
             temp = m.group(2).toString().toLowerCase();
             wordsCount.merge(temp, 1, Integer::sum);
         }
-        System.out.println(wordsCount);
-        return wordsCount;
-    }
-
-    public Map<String, Integer> getTop10Words(HashMap<String, Integer> m)
-    {
-        final Map<String, Integer> result = m
-                    .entrySet()
-                    .stream()
-                    .sorted(Map.Entry.<String, Integer> comparingByValue()
+        wordsCount = wordsCount.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer> comparingByValue()
                         .reversed()
                         .thenComparing(Map.Entry.comparingByKey()))
-                    .limit(10)
-                    .collect(Collectors.toMap(Map.Entry::getKey
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey
                         , Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-
-        System.out.println(result);
-        return result;
     }
 
-    public void getValueLines(String str)
+    public void getValueLines()
     {
         Pattern linePattern = Pattern.compile("\\S+");
-        Matcher matcher = linePattern.matcher(str);
+        Matcher matcher = linePattern.matcher(content);
         while (matcher.find()) {
             lineNumber++;
-            System.out.println(matcher.toString());
         }
     }
 
-    public void writeFile(String filePath, Map<String, Integer> hadSorted)
+    public void writeFile()
     {
         try {
-            File file = new File(filePath);
+            File file = new File(outputFilePath);
 
             if (!file.exists()) {
                 System.out.println("Error!");
@@ -107,7 +103,7 @@ public class WordCountUtil {
             bw.write("characters: " + charsNumber + '\n');
             bw.write("words: " + wordNumber + '\n');
             bw.write("lines: " + lineNumber + '\n');
-            for (Map.Entry<String, Integer> entry : hadSorted.entrySet()){
+            for (Map.Entry<String, Integer> entry : wordsCount.entrySet()){
 
                 bw.write(entry.getKey()+": "+entry.getValue()+'\n');
             }
