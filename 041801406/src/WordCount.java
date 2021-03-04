5bdbf24c3e8f244b8289e620c7f6e02c702aeb01
hzj2870757140 <79493364+hzj2870757140@users.noreply.github.com>
2021-03-04 18:45:20 +0800

    初步完成统计单词数量的功能V1.2

--- 041801406/src/WordCount.java
+++ 041801406/src/WordCount.java
@@ -5,6 +5,7 @@ public class WordCount {
         File file = new File("C:\\Users\\white\\Documents\\GitHub\\HOHO\\041801406\\src\\text.txt");
         int countChar = lib.countChar(file);
         int countLine = lib.countLine(file);
-        System.out.print(countChar + " " + countLine);
+        int countWord = lib.countWord(file);
+        System.out.print(countChar + " " + countLine + " " + countWord);
     }
 }
--- 041801406/src/lib.java
+++ 041801406/src/lib.java
@@ -1,71 +1,103 @@
 import java.io.*;
+import java.util.Calendar;
 
 public class lib {
-    static int countChar(File file)
-    {
+    static int countChar(File file) {
         int myCountChar = 0;
-        if(!file.exists())
-        {
-            return -1;
+        if (! file.exists()) {
+            return - 1;
         }
-        else
-        {
-            try
-            {
+        else {
+            try {
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);
-                while(br.read() != 1)
-                {
+                while (br.read() != 1) {
                     String s = br.readLine();
                     myCountChar += s.length();
+                    myCountChar++;
                 }
                 return myCountChar;
             }
-            catch(FileNotFoundException e)
-            {
+            catch (IOException e) {
                 e.printStackTrace();
             }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-            finally
-            {
+            finally {
                 return myCountChar;
             }
         }
     }
-    static int countLine(File file)
-    {
-        int myCountLine=0;
-        if(!file.exists())
-        {
-            return -1;
+
+    static int countLine(File file) {
+        int myCountLine = 0;
+        if (! file.exists()) {
+            return - 1;
         }
-        else
-        {
-            try
-            {
+        else {
+            try {
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);
-                while(br.readLine()!=null)
-                {
+                while (br.readLine() != null) {
                     myCountLine++;
                 }
                 return myCountLine;
             }
-            catch(FileNotFoundException e)
-            {
+            catch (IOException e) {
                 e.printStackTrace();
             }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-            finally
-            {
+            finally {
                 return myCountLine;
             }
         }
+    }
+
+    static int countWord(File file) {
+        if (! file.exists()) {
+            return - 1;
+        }
+        else {
+            int myWordCount = 0;
+            try {
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String s;
+                String word = "";
+                /*不使用.read()来判断 因为会读取首字母*/
+                while ((s = br.readLine()) != null) {
+                    for (int i = 0; i < s.length(); i++) {
+                        if (Character.isDigit(s.charAt(i)) || Character.isUpperCase(s.charAt(i)) || Character.isLowerCase(s.charAt(i))) {
+                            word += s.charAt(i);
+                        }
+                        else {
+                            if (isWord(word)) {
+                                myWordCount++;
+                            }
+                            word = "";
+                        }
+                    }
+                    if (isWord(word)) {
+                        myWordCount++;
+                    }
+                    word = "";
+                }
+                return myWordCount;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                return myWordCount;
+            }
+        }
+    }
+
+    static boolean isWord(String s) {
+        if (s.length() < 4)
+            return false;
+        for (int i = 0; i < 4; i++) {
+            if (! Character.isLowerCase(s.charAt(i)) && ! Character.isUpperCase(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
