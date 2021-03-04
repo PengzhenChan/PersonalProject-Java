3f705271d87f3d737038a8c510892d33eb201977
hzj2870757140 <79493364+hzj2870757140@users.noreply.github.com>
2021-03-04 18:42:25 +0800

    Revert "初步完成了统计字符的功能V1.0"
    This reverts commit b43295cc9771b961a45ecbbe9809f2aa088f6fe0.

--- src/WordCount.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public class WordCount {
-    public static void main(String args[]) {
-        File file = new File("C:\\Users\\white\\Documents\\GitHub\\HOHO\\041801406\\src\\text.txt");
-        int countChar = lib.countChar(file);
-        System.out.print(countChar);
-    }
-}
