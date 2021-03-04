2d3614e6d8c65ebddad9235da88b89e5c626a685
camocd <6410942@qq.com>
2021-03-04 16:33:38 +0800

    添加readFile函数

--- 221801230/src/Lib.java
+++ 221801230/src/Lib.java
@@ -1,5 +1,9 @@
-public class Lib {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Lib {
 
     private String inputFile;
     private String outputFile;
@@ -26,5 +30,20 @@ public class Lib {
 
     public void setOutputFile(String outputFile) {
         this.outputFile = outputFile;
+    }
+
+    public String readFile() {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            int ch;
+            while ((ch = reader.read()) != -1){
+                builder.append((char)ch);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 }
