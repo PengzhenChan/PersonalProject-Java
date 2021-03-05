2ae9ecd729f1a3e5f76adf1c8f3c33d35d6720ef
dqcx2411 <dqcx2411@outlook.com>
2021-03-05 09:52:24 +0800

    style : fix style

--- 031801124/src/Lib.java
+++ 031801124/src/Lib.java
@@ -12,43 +12,35 @@ public class Lib {
     public void countChars(String inputfileName, String outputfileName) {
         long charnumber = 0;
         try {
-
-            //System.out.println("文件字符数为"+charnumber);
-            // System.out.println("文件字符数统计成功！");
             File inputfile = new File(inputfileName);
-            Reader reader=null;
+            Reader reader = null;
             File outfile = new File(outputfileName);
-            reader= new InputStreamReader(new FileInputStream(inputfile));
+            reader = new InputStreamReader(new FileInputStream(inputfile));
             //if file doesnt exists, then create it
             int tempchar;
-            while ((tempchar=reader.read()) != -1) {
-                if(tempchar<=127&&tempchar>=0){
+            while ((tempchar = reader.read()) != -1) {
+                if (tempchar <= 127 && tempchar >= 0) {
                     charnumber++;
                 }
             }
             if (!outfile.exists()) {
                 outfile.createNewFile();
             }
-
-
-            //true = append file
             FileWriter fileWritter = new FileWriter(outfile, true);
             fileWritter.write("characters:" + Long.toString(charnumber));
             fileWritter.close();
-
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("文件打开失败");
         }
     }
 
     public void countWords(String inputfileName, String outputfileName) {
-        //Map<String,Integer> wordarray=new HashMap<String,Integer>();
         int wordnumber = 0;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(inputfileName));
             String strLine = null;
             int lineCount = 0;
-
             while (null != (strLine = bufferedReader.readLine())) {
                 int strindex = 0;
                 int strlength = strLine.length();
@@ -61,44 +53,28 @@ public class Lib {
                         wordlength++;
                         strindex++;
                     } else {
-
-
                         if (wordlength >= 4) {
                             wordnumber++;
                         }
                         strindex++;
                         wordlength = 0;
                     }
-
                 }
-
                 if (wordlength >= 4) {
                     wordnumber++;
                 }
-                if(strLine.length()!=0)
-                lineCount++;
-
-
             }
-
-
-            //System.out.println("文件单词数为" + wordnumber);
-            //System.out.println("文件单词数统计成功！");
             File outfile = new File(outputfileName);
-
             //if file doesnt exists, then create it
             if (!outfile.exists()) {
                 outfile.createNewFile();
             }
-
-            //true = append file
             FileWriter fileWritter = new FileWriter(outfile, true);
             fileWritter.write("\n" + "words:" + Integer.toString(wordnumber));
-            fileWritter.write("\n" + "lines:" + Integer.toString(lineCount));
             fileWritter.close();
 
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             System.out.println("文件打开失败");
         }
     }
@@ -124,7 +100,6 @@ public class Lib {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(inputfileName));
             String strLine = null;
             int lineCount = 0;
-
             while (null != (strLine = bufferedReader.readLine())) {
                 int strindex = 0;
                 int strlength = strLine.length();
@@ -137,22 +112,17 @@ public class Lib {
                         wordlength++;
                         strindex++;
                     } else {
-
-
                         if (wordlength >= 4) {
                             String tempword = strLine.substring(strindex - wordlength, strindex).toLowerCase();
-
                             if (wordarray.get(tempword) == null) {
                                 wordarray.put(tempword, 1);
                             } else {
                                 wordarray.put(tempword, wordarray.get(tempword) + 1);
                             }
-
                         }
                         strindex++;
                         wordlength = 0;
                     }
-
                 }
 
                 if (wordlength >= 4) {
@@ -165,8 +135,6 @@ public class Lib {
                     }
                 }
                 lineCount++;
-
-
             }
             List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(wordarray.entrySet()); //转换为list
             list.sort(new Comparator<Map.Entry<String, Integer>>() {
@@ -178,38 +146,53 @@ public class Lib {
                         return o1.getKey().compareTo(o2.getKey());
                 }
             });
-
-
             File outfile = new File(outputfileName);
-
             //if file doesnt exists, then create it
             if (!outfile.exists()) {
                 outfile.createNewFile();
             }
             FileWriter fileWritter = new FileWriter(outfile, true);
-
             if (list.size() < 10) {
                 for (int i = 0; i < list.size(); i++) {
-                    //System.out.println(list.get(i).getKey() + ": " + list.get(i).getValue());
-
                     fileWritter.write("\n" + list.get(i).getKey() + ": " + list.get(i).getValue());
-
                 }
             } else {
                 for (int i = 0; i < 10; i++) {
-                    //System.out.println(list.get(i).getKey() + ": " + list.get(i).getValue());
                     fileWritter.write("\n" + list.get(i).getKey() + ": " + list.get(i).getValue());
                 }
             }
             fileWritter.close();
-
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             System.out.println("文件打开失败");
         }
-
-
     }
 
+    public void countLines(String inputfileName, String outputfileName) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputfileName));
+            String strLine = null;
+            int lineCount = 0;
+            while (null != (strLine = bufferedReader.readLine())) {
+                for(int i=0;i<strLine.length();i++)
+                {
+                    if(strLine.charAt(i)!='\t'&&strLine.charAt(i)!='\r'&&strLine.charAt(i)!=' ') {
+                        lineCount++;
+                        break;
+                    }
+                }
+            }
+            File outfile = new File(outputfileName);
+            //if file doesnt exists, then create it
+            if (!outfile.exists()) {
+                outfile.createNewFile();
+            }
+            FileWriter fileWritter = new FileWriter(outfile, true);
+            fileWritter.write("\n" + "lines:" + Integer.toString(lineCount));
+            fileWritter.close();
+        } catch (Exception e) {
+            System.out.println("文件打开失败");
+        }
+    }
 }
 
--- 031801124/src/WordCount.java
+++ 031801124/src/WordCount.java
@@ -7,40 +7,33 @@ public class WordCount {
     public static void main(String[] args) {
         // write your code here
         Lib lib=new Lib();
-        //Scanner scan = new Scanner(System.in);
         String inputfilename="";
         String outputfilename="";
         if (args.length == 2) {
             inputfilename = args[0];
             outputfilename = args[1];
-        }
-
-            //System.out.println("测试用，请输入输入文件和输出文件。");
-            //String inputfilename = scan.nextLine();
-            //String outputfilename = scan.nextLine();
             try {
-                File inputfile = new File(inputfilename);
+                //File inputfile = new File(inputfilename);
                 File outputfile = new File(outputfilename);
                 if (!outputfile.exists()) {
                     outputfile.createNewFile();
                 }
-                FileWriter fileWritter = new FileWriter(outputfile);
+                FileWriter fileWritter = new FileWriter(outputfile); //用于清空output文件
                 fileWritter.close();
             } catch (Exception e) {
                 System.out.println("输入错误，文件打开失败");
                 System.exit(0);
             } finally {
-                //if file doesnt exists, then create it
-
-                lib.countChars(inputfilename,outputfilename);
-                lib.countWords(inputfilename,outputfilename);
-                lib.countMost(inputfilename,outputfilename);
+                lib.countChars(inputfilename, outputfilename);
+                lib.countWords(inputfilename, outputfilename);
+                lib.countLines(inputfilename, outputfilename);
+                lib.countMost(inputfilename, outputfilename);
             }
-
-        //} else {
-            //System.out.println("命令输入错误！");
-            //System.exit(0);
+        }
+         else {
+            System.out.println("命令输入错误！");
+            System.exit(0);
         }
 
     }
-//}
+}
