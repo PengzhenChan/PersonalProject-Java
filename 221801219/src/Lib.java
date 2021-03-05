8d8c455e448bd822be34173714284b0b5ccbf634
221801219 <18705919793@163.com>
2021-03-05 21:09:31 +0800

    fix countline bug

--- 221801219/src/Lib.java
+++ 221801219/src/Lib.java
@@ -7,26 +7,35 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Lib {
+    //统计字符数
     int charsNumber = 0;
+    //统计单词数
     int wordNumber = 0;
+    //统计有效行
     int lineNumber = 0;
+    //统计单词及其频率
     HashMap<String, Integer> wordsCount;
-    String content="";
-    String inputFilePath="";
-    String outputFilePath="";
+    //记录文本内容
+    String content = "";
+    //输入文件路径
+    String inputFilePath = "";
+    //输出文件路径
+    String outputFilePath = "";
 
-    Lib(String inputPath, String outputPath)
-    {
+    Lib(String inputPath, String outputPath) {
         inputFilePath = inputPath;
         outputFilePath = outputPath;
     }
+
     /**
-     * 读取文件并统计字符数
-     * @param filePath 文件路径
-     * @return 文件内容
+     * 打开文件并统计字符数
      */
-    public void readFileByChars() {
+    void readFileByChars() {
         File file = new File(inputFilePath);
+        if (!file.exists()) {
+            System.out.print("输入文件不存在，程序关闭。。。");
+            System.exit(0);
+        }
         //存储文件内容
         StringBuilder fileContent = new StringBuilder();
         charsNumber = 0;
@@ -36,103 +45,81 @@ public class Lib {
             while ((temp = reader.read()) != -1) {
                 fileContent.append((char) temp);
                 //不考虑汉字
-                if(temp < 128) {
+                if (temp < 128) {
                     charsNumber++;
                 }
             }
             content = fileContent.toString();
             reader.close();
         } catch (Exception e) {
-
-            e.printStackTrace();
+            System.out.print("e.getMessage()");
         }
     }
 
     /**
-     * 统计文本中的单词
-     * @param str 待处理文本
-     * @return 单词的哈希表
+     * 统计单词数量并排序
      */
-    public void countWords()
-    {
+    public void countWords() {
+        //匹配单词
         String pattern = "(^|[^A-Za-z0-9])([a-zA-Z]{4}[a-zA-Z0-9]*)";
-
-        // 创建 Pattern 对象
         Pattern r = Pattern.compile(pattern);
-        wordNumber = 0;
-        // 现在创建 matcher 对象
         Matcher m = r.matcher(content);
+
+        wordNumber = 0;
         wordsCount = new HashMap<String, Integer>();
         String temp;
-        while (m.find( )) {
+        while (m.find()) {
             wordNumber++;
-            temp = m.group(2).toString().toLowerCase();
+            //变为小写
+            temp = m.group(2).toLowerCase();
+            //若没有则添加，若有则加一
             wordsCount.merge(temp, 1, Integer::sum);
         }
-        wordsCount = wordsCount.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String, Integer> comparingByValue()
-                        .reversed()
+        //Stream操作
+        wordsCount = wordsCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
                         .thenComparing(Map.Entry.comparingByKey()))
-                .limit(10)
-                .collect(Collectors.toMap(Map.Entry::getKey
+                .limit(10).collect(Collectors.toMap(Map.Entry::getKey
                         , Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
     }
 
-    public void getValueLines()
-    {
+    /**
+     * 统计有效行
+     */
+    public void getValueLines() {
         lineNumber = 0;
-        Pattern linePattern = Pattern.compile("\\S+");
+        //匹配有效行
+        Pattern linePattern = Pattern.compile("(^|\n)\\s*\\S+");
         Matcher matcher = linePattern.matcher(content);
         while (matcher.find()) {
             lineNumber++;
         }
     }
 
-    public void writeFile()
-    {
+    /**
+     * 写入文件
+     */
+    public void writeFile() {
+        //打开文件
+        File file = new File(outputFilePath);
+        if (!file.exists()) {
+            System.out.print("输入文件不存在，程序关闭。。。");
+            System.exit(0);
+        }
         try {
-            File file = new File(outputFilePath);
-
-            if (!file.exists()) {
-                System.out.println("Error!");
-            }
-
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
-
-            bw.write("characters: " + charsNumber + '\n');
-            bw.write("words: " + wordNumber + '\n');
-            bw.write("lines: " + lineNumber + '\n');
-            for (Map.Entry<String, Integer> entry : wordsCount.entrySet()){
-
-                bw.write(entry.getKey()+": "+entry.getValue()+'\n');
+            bw.write("characters: " + charsNumber + '\n'
+            + "words: " + wordNumber + '\n'
+            + "lines: " + lineNumber + '\n');
+            for (Map.Entry<String, Integer> entry : wordsCount.entrySet()) {
+                bw.write(entry.getKey() + ": " + entry.getValue() + '\n');
             }
             bw.close();
-
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.print(e.getMessage());
         }
     }
-    public void writeFileTest(String filePath, String testStr)
-    {
-        try {
-            File file = new File(filePath);
-
-            if (!file.exists()) {
-                System.out.println("Error!");
-            }
-
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(testStr);
-            bw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
 
 
--- 221801219/src/WordCount.java
+++ 221801219/src/WordCount.java
@@ -1,20 +1,16 @@
 public class WordCount {
     public static void main(String[] args) {
-//        if(args.length != 2)
-//        {
-//            System.out.print("程序只接受两个参数");
-//            return;  //退出程序
-//        }
-//
-//        String inputFileName = args[0]; //输入文件名
-//        String outputFileName = args[1]; //输出文件名
-//        Lib wcu=new Lib(inputFileName, outputFileName);
-        Lib wcu=new Lib("C:\\Users\\Admin\\Desktop\\3.txt"
-                , "C:\\Users\\Admin\\Desktop\\4.txt");
+        if (args.length != 2) {
+            System.out.print("该程序只接受两个参数，待读取文件与待写入文件,程序关闭。。。");
+            System.exit(0);
+        }
+        String inputFilePath = args[0];
+        String outputFilePath = args[1];
+        //创建工具类执行方法
+        Lib wcu = new Lib(inputFilePath, outputFilePath);
         wcu.readFileByChars();
         wcu.countWords();
         wcu.getValueLines();
         wcu.writeFile();
-
     }
 }
