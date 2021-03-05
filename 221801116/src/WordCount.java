0d4d4aed4881f28b52278c67258f216af35007ad
csk <1649537125@qq.com>
2021-03-05 10:59:54 +0800

    11
    最终

--- 221801116/src/WordCount.java
+++ 221801116/src/WordCount.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -8,16 +9,28 @@ import java.util.concurrent.Future;
 
 public class WordCount {
     public static void main(String[] args){
-        File dir = new File(" ");
+        File dir = new File("...");
+        File input = new File(args[0]);
         String inputPath = null;
         String outputPath = null;
-        try{
-            inputPath = dir.getCanonicalPath()+"\\"+args[0];
-            outputPath = dir.getCanonicalPath()+"\\"+args[1];
-        }catch (IndexOutOfBoundsException | IOException e){
-            System.out.println("请检查是否正确输入命令行参数\n当前输入文件位置为\n"+inputPath);
-            System.exit(-1);
+        if(input.exists()){
+            try{
+                inputPath = args[0];
+                outputPath = args[1];
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("请检查是否正确输入命令行参数\n当前输入文件位置为\n"+inputPath);
+                System.exit(-1);
+            }
+        }else{
+            try{
+                inputPath = dir.getCanonicalPath()+args[0];
+                outputPath = dir.getCanonicalPath()+args[1];
+            }catch (IndexOutOfBoundsException | IOException e){
+                System.out.println("请检查是否正确输入命令行参数\n当前输入文件位置为\n"+inputPath);
+                System.exit(-1);
+            }
         }
+
 
 
         BufferedWriter bw = null;
@@ -25,6 +38,7 @@ public class WordCount {
             bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath),"UTF-8"));
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             System.out.println("错误位于WordCount类main方法,原因可能是未能正确创建output.txt文件");
+            System.exit(-1);
         }
         ExecutorService executorService = Executors.newCachedThreadPool();
         List<GetMyTask> taskList = new ArrayList<>();
