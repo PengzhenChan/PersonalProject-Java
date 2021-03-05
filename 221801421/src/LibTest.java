0b1d7829b8f7ba9cea3a5c867eb26d7e96a47042
unoni <523976222@qq.com>
2021-03-05 11:00:38 +0800

    性能测试

--- 221801421/src/LibTest.java
+++ 221801421/src/LibTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 import java.util.Map;
+import java.util.Random;
 
 public class LibTest {
 
@@ -52,5 +53,33 @@ public class LibTest {
         String str = WordCount.readFromFile("input.txt");
         Lib lib = new Lib(str);
         System.out.println(lib.getResult(10));
+    }
+
+    @Test
+    public void testPerformance(){
+        String str = WordCount.readFromFile("10000.txt");
+        Lib lib = new Lib(str);
+        System.out.println(lib.getResult(10));
+
+        /*String[] strings = {"apple1 ", "apple2 ", "banana3 ", "banana4 ", "golden5 ", "time6 ", "clover7 ", "clover8 ", "clover9 ", "clover10 ", "clover11 "};
+        int[] counts = new int[11];
+        int x=0;
+        int sum=0;
+        Random random = new Random(100);
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i=0;i<10000;i++){
+            x = random.nextInt(11);
+            stringBuffer.append(strings[x]);
+            if(x == 3 || x == 5) stringBuffer.append("\n");
+            counts[x]++;
+        }
+
+        WordCount.writeToFile("10000.txt", stringBuffer.toString());
+        for(int j=0;j<11;j++){
+            sum += counts[j];
+            System.out.println(counts[j]);
+        }
+        System.out.println(""+sum);*/
+
     }
 }
