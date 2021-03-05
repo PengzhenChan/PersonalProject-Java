1f894088d1edb56784322a8793c757d73e51883d
Weirdo341 <79393839+Weirdo341@users.noreply.github.com>
2021-03-05 20:17:58 +0800

    feat：需求功能的实现

--- 221801330/src/WordCount.java
+++ 221801330/src/WordCount.java
@@ -14,5 +14,35 @@ public class WordCount
             f1 = new File(args[0]);
             f2 = new File(args[1]);
         }
-		}    
+	    Lib lib = new Lib();
+        Lib.CharSum(f1);
+        Lib.FileRead(f1);
+        lib = Lib.GetData();
+        List<Map.Entry<String, Integer>> getWordFrequency = lib.GetWordFrequency();
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f2));
+            bw.write("characters: " + lib.GetCharSum() + "\n");
+            bw.write("words: " + lib.GetWordSum() + "\n");
+            bw.write("lines: " + lib.GetLineSum() + "\n");
+            if (getWordFrequency.size() < 10) {
+                for (int i = 0; i < getWordFrequency.size(); i++) {
+                    bw.write(getWordFrequency.get(i).getKey() + ": " + getWordFrequency.get(i).getValue());
+                    bw.write("\n");
+                }
+            }
+            else {
+                for (int i = 0; i < 10; i++) {
+                    bw.write(getWordFrequency.get(i).getKey() + ":" + getWordFrequency.get(i).getValue());
+                    bw.write("\n");
+                }
+            }
+            bw.close();
+        }
+        catch (IOException e) {
+        	System.err.println("发生异常" + e);
+            e.printStackTrace();
+        }
+        finally {
+        }
+    }    
 }
