6bdf6a169e1d36b0bc41d5e217b15b2f57d3fb74
Weirdo341 <79393839+Weirdo341@users.noreply.github.com>
2021-03-05 19:35:43 +0800

    feat: 新增字符数、单词数量、行数、单词出现频次的Set（）、Get（）方法

--- 221801330/src/Lib.java
+++ 221801330/src/Lib.java
@@ -1,1 +1,71 @@
+import java.io.*;
+import java.util.*;
 
+public class Lib {
+	private int charSum;
+	private int wordSum;
+	private int lineSum;
+	private HashMap<String, Integer> wordFrequency;
+	
+	Lib(){
+		charSum = 0;
+		wordSum = 0;
+		lineSum = 0;
+		wordFrequency = new HashMap<String,Integer>();
+	}
+	
+	void SetCharSum(int n) {
+	    charSum = n;
+	}
+	
+	int GetCharSum() {
+		return charSum;
+	}
+	
+	void SetWordSum(int n) {
+		wordSum += n;
+	}
+
+	int GetWordSum() {
+		return wordSum;
+	}
+	
+	void SetLineSum() {
+	    lineSum++;
+	}
+
+	void SetLineSum(int n) {
+		lineSum += n;
+	}
+
+	int GetLineSum() {
+		return lineSum;
+	}
+	
+	void SetWordFrequency(HashMap<String, Integer> hash) {
+		for (String s : hash.keySet()) {
+			if (wordFrequency.containsKey(s)) {
+				wordFrequency.put(s, wordFrequency.get(s) + hash.get(s));
+			}
+			else {
+				wordFrequency.put(s, 1);
+			}
+		}
+	}
+
+	List<Map.Entry<String, Integer>> GetWordFrequency() {
+		List<Map.Entry<String, Integer>> list = null;
+		list = new ArrayList<Map.Entry<String, Integer>>(wordFrequency.entrySet());
+		list.sort(new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				if (o1.getValue() != o2.getValue()) {
+					return o2.getValue().compareTo(o1.getValue());
+				}
+				else {
+					return o1.getKey().compareTo(o2.getKey());
+				}
+			}
+		});
+		return list;
+	}
+}
