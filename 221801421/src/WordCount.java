bffbaf149884fdfd70cbe7c58b5c9b4199d810d3
unoni <523976222@qq.com>
2021-03-05 10:57:14 +0800

    完成了有效行数的统计

--- 221801421/src/WordCount.java
+++ 221801421/src/WordCount.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordCount {
-
     private static String DIR = System.getProperty("user.dir");
+    //空白行会成为两个\s*的一部分
+    private static String LINE_REGEX = "\\s*\\S+\\s*\n";
+    private static String WORD_REGEX = "";
 
     private long charNum = 0;
     private long lineNum = 0;
@@ -37,6 +41,7 @@ public class WordCount {
     public long charsCount(String str){
         long count = 0;
 
+        //题目中说明了没给汉字,所以其实直接用str.length()也行
         char[] ch = str.toCharArray();
         for(int i=0;i<ch.length;i++){
             if(ch[i] <= 127){
@@ -47,9 +52,20 @@ public class WordCount {
         return count;
     }
 
+    public long linesCount(String str){
+        long count = 0;
+
+        Matcher matcher = Pattern.compile(LINE_REGEX).matcher(str);
+        while(matcher.find()){
+            count++;
+        }
+        return count;
+    }
+
     public static void main(String[] args){
         WordCount wordCount = new WordCount();
         String str = readFromFile("input.txt");
-        System.out.println(wordCount.charsCount(str));
+        //System.out.println(wordCount.charsCount(str));
+        //System.out.println(wordCount.linesCount(str));
     }
 }
