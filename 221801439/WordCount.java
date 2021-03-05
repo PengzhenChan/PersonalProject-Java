b3ae66d7414d237a51a3c6359b9de6ba61e1e4b8
lulili666 <2193683114@qq.com>
2021-03-05 17:01:11 +0800

    单词数的统计

--- 221801439/WordCount.java
+++ 221801439/WordCount.java
@@ -32,11 +32,14 @@ public class tt{
 					lines++;			
 				}	
 				characters += countCharacters(str);  //对字符统计数累加
+				word += countWords(str);  //对单词统计数累加
 			}	
 			System.out.println(characters);
 			sb.append("characters:" + characters + "\n");
 			System.out.println(lines);
 			sb.append("lines:" + lines + "\n");
+			System.out.println(word);
+			sb.append("word:" + word + "\n");
 			
 			reader.close();
 			FileWriter fw = new FileWriter("src/test/output.txt");  //写入文件
@@ -60,12 +63,30 @@ public class tt{
     public static int countCharacters(String str) 
     {
         int count = 0;
-        Pattern p = Pattern.compile("[&nbsp;-~]");
+        Pattern p = Pattern.compile("[\\x00-\\x7F]");
         Matcher m = p.matcher(str);
         while(m.find()){
             count++;
         }
         return count;
     }
+    
+    /**
+  	 * 统计单词数
+  	 * @param str
+  	 * @return count
+  	 */
+      public static int countWords(String str)
+      {
+      	 int count = 0;
+      	 String ss = str.toLowerCase();
+      	 Pattern p = Pattern.compile("[a-z]{4}[0-9a-z]*");    	 
+           Matcher m = p.matcher(ss);
+           while(m.find())
+           {
+               count++;
+           }
+  		return count;
+      }
 	
 }
