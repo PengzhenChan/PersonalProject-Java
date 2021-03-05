04a18b911cab42a562f18d8d2d843fc9d241ef91
Weirdo341 <79393839+Weirdo341@users.noreply.github.com>
2021-03-05 19:10:31 +0800

    feat: 实现命令行输入以及输入异常状态下的判断

--- 221801330/src/WordCount.java
+++ 221801330/src/WordCount.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.*;
 
+public class WordCount
+{
+    public static void main(String[] args){
+		    File f1;
+		    File f2;
+		    if (args.length < 2) {
+            f1 = new File("F:/input.txt");
+            f2 = new File("F:/output.txt");
+        }
+		    else {
+            f1 = new File(args[0]);
+            f2 = new File(args[1]);
+        }
+		}    
+}
